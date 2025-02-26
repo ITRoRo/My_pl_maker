6cda5d44deba98821d95a3a42c265c019a5c7d8e
ITRoRo <roman.roschin2014@gmail.com>
2025-02-26 21:40:50 +0300

    Revert "очередной"

--- app/src/main/java/com/example/myplmaker/MainActivity.kt
+++ app/src/main/java/com/example/myplmaker/MainActivity.kt
@@ -1,65 +1,20 @@
 package com.example.myplmaker
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        /*
-                val image = findViewById<Button>(R.id.search)
-                image.setOnClickListener {
-                    val displayIntent = Intent(this, SearchActivity::class.java)
-                    startActivity(displayIntent)
-                }
-        */
-
-
-
-        val image = findViewById<Button>(R.id.search)
-        val displayIntent = Intent(this, SearchActivity::class.java)
-        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
-
-            override fun onClick(v: View?) {
-                startActivity(displayIntent)
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-
-        image.setOnClickListener(imageClickListener)
-
-
-
-
-
-
-
-        val media = findViewById<Button>(R.id.mmedia)
-        media.setOnClickListener {
-            val mediaIntent = Intent(this, MediaActivity::class.java)
-            startActivity(mediaIntent)
-        }
-
-        val setting = findViewById<Button>(R.id.settings)
-        setting.setOnClickListener {
-            val settingIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(settingIntent)
-        }
-
-
-
-
-
-
     }
 }
