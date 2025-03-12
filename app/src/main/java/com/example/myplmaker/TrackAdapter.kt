b544d44dcc68c9d598055b17bfa7ba99ea0f0b22
Роман <roman.roschin2014@yandex.ru>
2025-03-12 22:46:09 +0300

    Спринт 10

---
+++ app/src/main/java/com/example/myplmaker/TrackAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.myplmaker
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class TrackAdapter (private val trackListAd : ArrayList<Track>) : RecyclerView.Adapter<TrackHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackHolder, position: Int) {
+        holder.bind(trackListAd[position])
+    }
+
+    override fun getItemCount(): Int {
+        return trackListAd.size
+    }
+}
